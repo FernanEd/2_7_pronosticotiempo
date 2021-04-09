d6f59c983d0d367e4b5bdc071394fc50a7ed1c51
FernanEd <fernandoed.bm@gmail.com>
2021-04-08 22:18:55 -0500

    done

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const API_KEY = "653ada71ed7b4d56228b06e2f0fc17b5";
-const cityName = "tampico";
-const displayedDays = 15;
+const tampico = {
+  lat: 22.3,
+  lon: -97.85,
+};
 
 const getWeatherData = async () => {
   let res = await fetch(
-    `api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&cnt=${displayedDays}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${tampico.lat}&lon=${tampico.lon}&exclude=hourly,minutely,alerts&appid=${API_KEY}`
   );
   let data = await res.json();
+  populateContent(data.daily.map((dia) => dia.temp.day));
+};
 
-  console.log(data);
+const populateContent = (clima7Dias) => {
+  const $wrapper = document.querySelector("#pronostico-wrapper");
+  $wrapper.innerHTML = clima7Dias
+    .map((kelvin, i) => {
+      let clima = Math.floor((kelvin - 273.15) * 10) / 10;
+      if (i === 0) {
+        return `<div class='pronostico-item'>Hoy: ${clima}°C</div>`;
+      } else {
+        return `<div class='pronostico-item'>En ${i} dia(s): ${clima}°C</div>`;
+      }
+    })
+    .join("");
 };
 
 getWeatherData();
